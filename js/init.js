fd3c80b7bc45f83421ca5e64d1adb2b03d2d3e20
cedv1990 <cedv90@gmail.com>
2020-05-15 18:54:15 -0500

    Carga asíncrona
    Carga hasta que exista document.body y hasta que exista la función de las cookies

--- js/init.js
+++ js/init.js
@@ -1,98 +1,120 @@
 ﻿(w => {
-    const footer = s5('footer.bottom').shift();
-    const nav = s5('nav').cloneNode(true);
-    const sideBar = s5('.sidebar-menu').shift().insert(nav);
-    const container = s5('.content').shift();
-    const navLinks = s5('.nav-link');
-    const currentMode = document.body.className;
-    const modal = s5('modal');
-      
-    const cargaInicial = () => {
-        nav.removeAttribute('id');
-        nav.classList.add('main-menu');
+    const fnStart = () => {
 
-        w['onLoadEnd'] = () => footer.classList.remove('bottom');
+        const footer = s5('footer.bottom').shift();
+        const nav = s5('nav').cloneNode(true);
+        const sideBar = s5('.sidebar-menu').shift().insert(nav);
+        const sideBarBg = s5('.sidebar-menu-bg').shift();
+        const container = s5('.content').shift();
+        const navLinks = s5('.nav-link');
+        const currentMode = document.body.className;
+        const modal = s5('modal');
 
-        s5('.year').forEach(el => el.innerHTML = new Date().getFullYear());
+        const fnNavBar = () => {
+            sideBarBg.classList.toggle('visible');
+            sideBar.classList.toggle('visible');
+        };
 
-        const asignarVersion = ({app}) => s5('.version').forEach(el => el.innerHTML = `versión: ${app}`);
+        const cargaInicial = () => {
+            nav.removeAttribute('id');
+            nav.classList.add('main-menu');
 
-        if (!window['app-version']) {
-            s5.watch(window, 'app-version', () => asignarVersion(window['app-version']));
-        }
-        else {
-            asignarVersion(window['app-version']);
-        }
-        
-        s5('a[mode]').addEvent('click', e => {
-            const mode = e.target.getAttribute('mode');
-            w['writeCookie']('view', mode);
-            w.location.reload();
-        });
-        s5('.navbar-button').addEvent('click', () => sideBar.classList.toggle('visible'));
-        s5('.nav-link[mode]').forEach(t => {
-            if (t.attribute('mode') === currentMode) {
-                t.parentNode.classList.add('theme');
+            w['onLoadEnd'] = () => footer.classList.remove('bottom');
+
+            s5('.year').forEach(el => el.innerHTML = new Date().getFullYear());
+
+            const asignarVersion = ({ v1, v2, app }) => {
+                s5('.version').forEach(el => el.innerHTML = `versión: ${app}`);
+                s5('.v1').forEach(el => el.innerHTML = v1);
+                s5('.v2').forEach(el => el.innerHTML = v2);
+            };
+
+            if (!window['app-version']) {
+                s5.watch(window, 'app-version', () => asignarVersion(window['app-version']));
             }
             else {
-                t.parentNode.classList.remove('theme');
+                asignarVersion(window['app-version']);
             }
-        });
-        s5('.current-theme').forEach(themeIcon => {
-            themeIcon.className = '';
-            themeIcon.className = `fa fa-${currentMode === 'dark' ? 'moon' : 'sun'}`;
 
-            themeIcon.parentNode.setAttribute('title', `Tema actual: ${currentMode}`);
-        });
+            s5('a[mode]').addEvent('click', e => {
+                const mode = e.target.getAttribute('mode');
+                w['writeCookie']('view', mode);
+                w.location.reload();
+            });
+            s5('.navbar-button').addEvent('click', fnNavBar);
+            sideBarBg.addEvent('click', fnNavBar);
+            s5('.nav-link[mode]').forEach(t => {
+                if (t.attribute('mode') === currentMode) {
+                    t.parentNode.classList.add('theme');
+                }
+                else {
+                    t.parentNode.classList.remove('theme');
+                }
+            });
+            s5('.current-theme').forEach(themeIcon => {
+                themeIcon.className = '';
+                themeIcon.className = `fa fa-${currentMode === 'dark' ? 'moon' : 'sun'}`;
 
-        s5('.img-logo > img').forEach(img => img.attribute('src', `images/${currentMode}-Logo_S5.png`));
-    };
+                themeIcon.parentNode.setAttribute('title', `Tema actual: ${currentMode}`);
+            });
 
-    const cargaModulos = () => {
-        const modules = ['index', 'components', 'who', 'code'];
-        w['app'] = s5.initialize(null, null, 'js');
-        w['app'].require([...modules], (...loadedModules) => {
+            s5('.img-logo > img').forEach(img => img.attribute('src', `images/${currentMode}-Logo_S5.png`));
+        };
 
-            const limpiar = () => {
-                container.innerHTML = '';
-                modal.close();
+        const cargaModulos = () => {
+            const modules = ['index', 'components', 'who', 'code'];
+            w['app'] = s5.initialize(null, null, 'js');
+            w['app'].require([...modules], (...loadedModules) => {
 
-                s5('.current').forEach(c => c.classList.remove('current'));
-            };
+                const limpiar = () => {
+                    container.innerHTML = '';
+                    modal.close();
 
-            const stickyTitle = container => {
-                const selector = container.id ? `#${container.id}` : `.${container.className.split(' ').join('.')}`;
-                const elements = s5(`${selector} > [class*="title"], ${selector} > [id*="title"]`);
-                s5.map(elements, el => el.classList.add('sticky-title'));
-                if (elements.length > 0)
-                    elements[0].nextSibling.classList.add('sticky-sibling');
-            };
+                    s5('.current').forEach(c => c.classList.remove('current'));
+                };
 
-            const navegar = () => {
-                limpiar();
+                const stickyTitle = container => {
+                    const selector = container.id ? `#${container.id}` : `.${container.className.split(' ').join('.')}`;
+                    const elements = s5(`${selector} > [class*="title"], ${selector} > [id*="title"]`);
+                    s5.map(elements, el => el.classList.add('sticky-title'));
+                    if (elements.length > 0)
+                        elements[0].nextSibling.classList.add('sticky-sibling');
+                };
 
-                let option = w.location.hash || '#index';
-                navLinks.filter(n => n.attribute('href') === option)
+                const navegar = () => {
+                    limpiar();
+
+                    let option = w.location.hash || '#index';
+                    navLinks.filter(n => n.attribute('href') === option)
                         .forEach(el => el.parentNode.classList.add('current'));
-                option = option.split('-');
+                    option = option.split('-');
 
-                const nombreModulo = option.shift().replace('#', '');
+                    const nombreModulo = option.shift().replace('#', '');
 
-                const design = loadedModules[modules.indexOf(nombreModulo)].init(option.shift());
+                    const design = loadedModules[modules.indexOf(nombreModulo)].init(option.shift());
 
-                sideBar.classList.remove('visible');
-            
-                container.insert(design);
+                    sideBar.classList.remove('visible');
+                    sideBarBg.classList.remove('visible');
 
-                stickyTitle(design);
-            };
+                    container.insert(design);
 
-            navegar();
+                    stickyTitle(design);
+                };
 
-            s5.addEvent.call(w, 'hashchange', navegar);
-        });
+                navegar();
+
+                s5.addEvent.call(w, 'hashchange', navegar);
+            });
+        };
+
+        cargaInicial();
+        cargaModulos();
     };
 
-    cargaInicial();
-    cargaModulos();
+    if (document.body.loaded) {
+        fnStart();
+    }
+    else {
+        s5.watch(document.body, 'loaded', fnStart);
+    }
 })(window);
--- js/theme-chooser.js
+++ js/theme-chooser.js
@@ -1,26 +1,39 @@
 ﻿(w => {
-    let mode = w['readCookie']('view') || 'light';
-    const hashes = w.location.hash.split('#');
-    const comparer = h => h.includes('theme');
-    const metas = document.querySelectorAll('.meta-theme');
-    let color = '#f8f8f8';
+    const fn = () => {
+        let mode = w['readCookie']('view') || 'light';
+        const hashes = w.location.hash.split('#');
+        const comparer = h => h.includes('theme');
+        const metas = document.querySelectorAll('.meta-theme');
+        let color = '#f8f8f8';
 
-    if (hashes.some(comparer)) {
-        mode = hashes.find(comparer).split('=')[1];
-        w['writeCookie']('view', mode);
-        w.location.href = w.location.href
-            .replace(`#theme=${mode}`, '')
-            .replace(`%23theme=${mode}`, '');
-    }
-    document.body.className = '';
-    if (mode === 'light') {
-        w['writeCookie']('view', 'light');
-        document.body.classList.add('light');
-    }
-    else {
-        document.body.classList.add('dark');
-        document.head.querySelector('#theme').href = 'css/variables/dark.css';
-        color = '#0a0a0a';
-    }
-    metas.forEach(m => m.setAttribute('content', color));
+        if (hashes.some(comparer)) {
+            mode = hashes.find(comparer).split('=')[1];
+            w['writeCookie']('view', mode);
+            w.location.href = w.location.href
+                .replace(`#theme=${mode}`, '')
+                .replace(`%23theme=${mode}`, '');
+        }
+        document.body.className = '';
+        if (mode === 'light') {
+            w['writeCookie']('view', 'light');
+            document.body.classList.add('light');
+        }
+        else {
+            document.body.classList.add('dark');
+            document.head.querySelector('#theme').href = 'css/variables/dark.css';
+            color = '#0a0a0a';
+        }
+        metas.forEach(m => m.setAttribute('content', color));
+
+        document.body.loaded = true;
+    };
+
+    let inte;
+    inte = setInterval(() => {
+        if (document.body && w['readCookie']) {
+            clearInterval(inte);
+            fn();
+        }
+    }, 1);
+
 })(window);
