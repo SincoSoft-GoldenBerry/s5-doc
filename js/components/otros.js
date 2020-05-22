b28c3d44ac159348ec4425ea1b9c2374dede199b
cedv1990 <cedv90@gmail.com>
2020-05-22 13:22:46 -0500

    Nuevo menu Otros
    Espacio para SincoInitializationError, isIE, etc

---
+++ js/components/otros.js
@@ -0,0 +1,8 @@
+﻿window['app'].define('components/otros', [], () => {
+    return {
+        get: () => {
+            //Espacio para SincoInitializationError, isIE, etc
+            return s5('<div>', { 'class': 'proto-container' });
+        }
+    }
+});
