83204035ffb30794f280dfab82616f2d8a76843b
cedv1990 <cedv90@gmail.com>
2020-05-29 20:17:52 -0500

    Create remote-debugger.ps1
    Pasos para depurar dispositivos iOS en Windows
    
    Co-Authored-By: S5 <sincosoft-goldenberry@users.noreply.github.com>

---
+++ remote-debugger.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+#en cmd: remotedebug_ios_webkit_adapter --port=9000
+#en chrome abrir esta url en una pestaña: chrome://inspect/#devices
+#si no aparece el dispositivo, registrar en Configure: localhost:9000
